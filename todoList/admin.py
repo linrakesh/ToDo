0d8736c79c012f6b75864b2a6795902824ca1344
rakesh <rakesh.linux@gmail.com>
2019-06-20 17:26:41 +0530

    admin panel using django suit

--- todoList/admin.py
+++ todoList/admin.py
@@ -2,4 +2,13 @@ from django.contrib import admin
 from .models import List
 
 # Register your models here.
-admin.site.register(List)
+class ListAdmin(admin.ModelAdmin):
+    date_hierarchy = 'update_on'
+    fields = ('title', 'desc','update_on')  # This will show fields on admin Panel Update/inert
+    list_display = ('title','desc','update_on')
+    list_filter = ['title','update_on']
+    search_fields = ['title','desc']
+    list_per_page = 3
+    ordering = ['-update_on'] # '-' is used to display oject in descending order
+
+admin.site.register(List,ListAdmin)
